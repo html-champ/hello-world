e1bbb183b5a51e1fa207a152851e222ce9ef0bb7
html-champ <39513847+html-champ@users.noreply.github.com>
2018-07-29 13:30:47 +0200

    Add files via upload

--- js/devicemotion.js
+++ js/devicemotion.js
@@ -1,18 +1,11 @@
-// Inhalt des Scripts devicemotion.js:
-maxi = -1;
 if (window.DeviceMotionEvent) {
-    document.getElementById("dmeSupported").innerText = "OK - Device Motion wird unterstützt!";
+    document.getElementById("dmeSupported").innerText = "OK -Device Motion wird unterstützt!";
     window.addEventListener('devicemotion', function(event) {
         document.getElementById("xBeschl").innerHTML = 'x ' + event.acceleration.x.toFixed(2);
-        document.getElementById("yBeschl").innerHTML = 'y ' + event.acceleration.y.toFixed(2);
-        document.getElementById("zBeschl").innerHTML = 'z ' + event.acceleration.z.toFixed(2);
-
-        if (event.acceleration.z > maxi) {
-            maxi = event.acceleration.z;
-            document.getElementById("meinMax").innerHTML = 'max z: ' + maxi;
-        }
-
+        document.getElementById("xBeschl").innerHTML = 'y ' + event.acceleration.y.toFixed(2);
+        document.getElementById("xBeschl").innerHTML = 'z ' + event.acceleration.z.toFixed(2);
     });
+
 } else {
-    document.getElementById("dmeSupported").innerText = "Device Motion wird nicht unterstützt!";
+    document.getElementById("dmeSupported").innerText = "Device Motion wird nicht unterstützt!";
 }
