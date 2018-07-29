0490e1da3a1556e3ab5ce4842cbdb5a2f442eed1
html-champ <39513847+html-champ@users.noreply.github.com>
2018-07-29 15:36:33 +0200

    Add files via upload

---
+++ js/devicemotion1.js
@@ -0,0 +1,12 @@
+if (window.DeviceMotionEvent) {
+    document.getElementById("dmeSupported").innerText = "OK - Device Motion Rot wird unterstützt!";
+    window.addEventListener('devicemotion', function(event) {
+
+        document.getElementById("xRotation").innerHTML = 'Rotationsrate beta um x-Achse: ' + event.rotationRate.alpha.toFixed(2);
+        document.getElementById("yRotation").innerHTML = 'Rotationsrate gamma um y-Achse: ' + event.rotationRate.beta.toFixed(2);
+        document.getElementById("zRotation").innerHTML = 'Rotationsrate alpha um z-Achse: ' + event.rotationRate.gamma.toFixed(2);
+
+    });
+} else {
+    document.getElementById("dmeSupported").innerText = "Device Motion wird nicht unterstützt!";
+}
