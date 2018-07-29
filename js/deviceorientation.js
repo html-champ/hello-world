3d83ba55df61cc8f7ad3b599ebdceab68cc701e6
html-champ <39513847+html-champ@users.noreply.github.com>
2018-07-29 14:25:41 +0200

    Add files via upload

---
+++ js/deviceorientation.js
@@ -0,0 +1,11 @@
+if (window.DeviceOrientationEvent) {
+    document.getElementById("doeSupported").innerText = "Supported!";
+    window.addEventListener('deviceorientation', deviceOrientationHandler, false); {
+        document.getElementById("xRot").innerHTML = 'x1 ' + event.acceleration.x1.toFixed(2);
+        document.getElementById("yRot").innerHTML = 'y1 ' + event.acceleration.y1.toFixed(2);
+        document.getElementById("zRot").innerHTML = 'z1 ' + event.acceleration.z1.toFixed(2);
+    };
+
+} else {
+    document.getElementById("deviceorientation").innerText = "Device Rotation wird nicht unterstützt!";
+};
