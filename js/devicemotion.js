28f99da561845c398008a40f3024cd8627e11ffe
html-champ <39513847+html-champ@users.noreply.github.com>
2018-07-29 16:42:26 +0200

    Add files via upload

--- js/devicemotion.js
+++ js/devicemotion.js
@@ -4,6 +4,10 @@ if (window.DeviceMotionEvent) {
         document.getElementById("xBeschl").innerHTML = 'Beschleunigung in x-Richtung ' + event.acceleration.x.toFixed(1);
         document.getElementById("yBeschl").innerHTML = 'Beschleunigung in y-Richtung ' + event.acceleration.y.toFixed(1);
         document.getElementById("zBeschl").innerHTML = 'Beschleunigung in z-Richtung ' + event.acceleration.z.toFixed(1);
+
+        document.getElementById("xRotation").innerHTML = 'Rotationsrate beta um x-Achse: ' + event.rotationRate.alpha.toFixed(2);
+        document.getElementById("yRotation").innerHTML = 'Rotationsrate gamma um y-Achse: ' + event.rotationRate.beta.toFixed(2);
+        document.getElementById("zRotation").innerHTML = 'Rotationsrate alpha um z-Achse: ' + event.rotationRate.gamma.toFixed(2);
     });
 
 } else {
